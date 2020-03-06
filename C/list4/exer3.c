41086028c6dd2ac2141688d204e91b166ff2e465
CarlosSantosJr <carlos.dos.santos@hotmail.com>
2020-03-05 22:36:10 -0300

    reorganização de pastas

--- C/list4/exer3.c
+++ C/list4/exer3.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 
-int my_strlen(char *str) {
+int my_putstr(char *a) {
 	int i;
 
-	for(i = 0; str[i] != '\0'; i++);
+	for(i = 0; a[i] != '\0'; i++);
 
 	return i;
 }
 
 int main() {
+	char *str = "hello";
+
 	 printf("%d", my_strlen("testando"));
 	 return 0;
 }
+
+
+
